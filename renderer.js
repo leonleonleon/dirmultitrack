2e78d54d72a24a48abfe0585f40bc20a20703068
Leon Reindl <info@leonreindl.de>
2019-05-02 00:27:58 +0200

    add open finder thing

--- renderer.js
+++ renderer.js
@@ -1,7 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const fs = require('fs');
+const fs = require( 'fs' );
+const {shell} = require('electron')
 
 const target = document.getElementById( 'target' );
 const currentDir = document.getElementById( 'currentDir' );
@@ -63,7 +64,7 @@ function checkForR16( path, rootDir ) {
 
         // first = false;
 
-        createPlayer( path, rootDir, 'AUDIO' );
+        createPlayer( path, rootDir, 'AUDIO', 'Zoom R16' );
 
     }
 }
@@ -72,14 +73,25 @@ function checkFor95000( path, rootDir ) {
     const projectFile = rootDir + '/' + path + '/TEMPO.TXT';
 
     if ( fs.existsSync( projectFile ) && path ) {
-        createPlayer( path, rootDir, '' );
+        const info = get9500ProjectInfo( projectFile );
+        createPlayer( path, rootDir, '', 'Ehx95000 ' + info );
     }
 }
 
+function get9500ProjectInfo( projectFile ) {
 
-function createPlayer( path, rootDir, audiopath ) {
+    const infoRaw = fs.readFileSync( projectFile, 'utf8' );
+    const info = infoRaw.match(/Tempo=( |\d+)(\d+)(.)(\d+)( )bpm/)[0].replace('Tempo=', '');
+
+    return info;
+}
+
+
+function createPlayer( path, rootDir, audiopath, projectInfo ) {
 
     const projectElement = document.createElement( 'div' );
+
+    projectElement.classList.add( 'project' );
 
     let audioList = '<ul>';
     let player = '<div class="player">';
@@ -109,8 +121,12 @@ function createPlayer( path, rootDir, audiopath ) {
         player = player + '</div>';
         audioList = audioList + '</ul>';
 
+        if ( projectInfo ) projectInfo = '| ' +  projectInfo;
+
+        const openLink = ' <button class="opendir" data-dir="' + audioDir + '">open</button>';
+
         // projectElement.innerHTML = '<h3>' + path + '</h3>' + audioList + '<div><button class="play">play</button></div>';
-        projectElement.innerHTML = '<h3>' + path + '</h3>' + player;
+        projectElement.innerHTML = '<header><h3>' + path + ' ' + projectInfo + '</h3><div class="buttonwrapper">' + openLink + '</div></h3>' + player;
 
         if ( hasSound ) {
             target.appendChild( projectElement );
@@ -118,7 +134,14 @@ function createPlayer( path, rootDir, audiopath ) {
 
         jQuery(document).ready(function() {
             jQuery(".player").trackSwitch(); // All other players are default
+            jQuery('.opendir').click('click', ( e ) => {
+                openFolder( jQuery(e.target).data('dir') );
+            })
         });
 
     } );
+}
+
+function openFolder( path ){
+    shell.openItem( path );
 }
