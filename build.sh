90648f7c6ac45e324331c91fd66f6a152e585885
Leon Reindl <info@leonreindl.de>
2019-05-02 19:54:49 +0200

    update build

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@ cp index.html build/DIR-Multitrack.app/Contents/Resources/app/index.html
 cp package.json build/DIR-Multitrack.app/Contents/Resources/app/package.json
 cp renderer.js build/DIR-Multitrack.app/Contents/Resources/app/renderer.js
 cp main.js build/DIR-Multitrack.app/Contents/Resources/app/main.js
+cd build/DIR-Multitrack.app/Contents/Resources/app/
+npm install --production
+cd ../../../../../
 zip -r build/DIR-Multitrack.app{.zip,}
