d0352eabc6744a36e14065a1dc5b1544d39770a3
Leon Reindl <info@leonreindl.de>
2019-05-02 23:05:56 +0200

    fix

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@ cp index.html build/DIR-Multitrack.app/Contents/Resources/app/index.html
 cp package.json build/DIR-Multitrack.app/Contents/Resources/app/package.json
 cp renderer.js build/DIR-Multitrack.app/Contents/Resources/app/renderer.js
 cp main.js build/DIR-Multitrack.app/Contents/Resources/app/main.js
+<<<<<<< HEAD
 cd build/DIR-Multitrack.app/Contents/Resources/app/
 npm install --production
 cd ../../../../../
 zip -r build/DIR-Multitrack.app{.zip,}
+=======
+>>>>>>> parent of 8782c0c... add zipped app
